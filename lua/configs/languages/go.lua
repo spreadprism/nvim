0243d5839d17e37e0ba38e80cff5d990c1eb73e7
spreadprism <sprism@proton.me>
2024-09-26 10:37:10 -0400

    moved go configs

--- lua/configs/languages/go.lua
+++ lua/configs/languages/go.lua
@@ -1,3 +1,14 @@
+lsp("gopls")
+formatter("go", "gofmt"):install(false)
+launch_configs("go", {
+	{
+		name = "Launch current file",
+		type = "go",
+		request = "launch",
+		program = "${file}",
+	},
+})
+
 plugin("leoluz/nvim-dap-go")
 	:ft("go")
 	:dependencies({ "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" })
@@ -9,14 +20,3 @@ plugin("leoluz/nvim-dap-go")
 		})
 		require("internal.dap").refresh_configurations("go") -- INFO: Removes the default configs
 	end)
-
-lsp("gopls")
-formatter("go", "gofmt"):install(false)
-launch_configs("go", {
-	{
-		name = "Launch current file",
-		type = "go",
-		request = "launch",
-		program = "${file}",
-	},
-})
