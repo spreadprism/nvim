9b60c581d631c25581ddb5bca27aa48875d2c9f9
spreadprism <sprism@proton.me>
2024-09-26 10:27:09 -0400

    Mason config cleanup + missing linting configs

--- lua/configs/core/mason.lua
+++ lua/configs/core/mason.lua
@@ -1,15 +1,9 @@
--- TODO: This should be empty
-local ensure_installed = {
-	"ruff",
-	"prettier",
-	"stylua",
-}
 plugin("williamboman/mason.nvim")
 	:lazy(false)
 	:dependencies({
-		plugin("williamboman/mason-lspconfig.nvim"),
-		plugin("jay-babu/mason-nvim-dap.nvim"),
-		plugin("WhoIsSethDaniel/mason-tool-installer.nvim"),
+		"williamboman/mason-lspconfig.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	})
 	:config(function()
 		require("mason").setup({
@@ -21,11 +15,15 @@ plugin("williamboman/mason.nvim")
 		})
 		require("mason-lspconfig").setup()
 		require("mason-nvim-dap").setup()
-		--INFO: lsp
+		local ensure_installed = {}
+		-- INFO: Lsp
 		ensure_installed = vim.list_extend(ensure_installed, require("internal.lsp").mason())
-		--INFO: dap
+		-- INFO: Dap
 		ensure_installed = vim.list_extend(ensure_installed, require("internal.dap").mason())
+		-- INFO: Formatters
 		ensure_installed = vim.list_extend(ensure_installed, require("internal.formatting").mason())
+		-- INFO: Linters
+		ensure_installed = vim.list_extend(ensure_installed, require("internal.linting").mason())
 		require("mason-tool-installer").setup({
 			ensure_installed = ensure_installed,
 		})
