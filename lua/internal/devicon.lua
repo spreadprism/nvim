d0ad9075f1d1ba3531c1f4932a6f532c77f1ff9e
spreadprism <sprism@proton.me>
2024-10-01 11:36:03 -0400

    fixed bug with icon patterns

--- lua/internal/devicon.lua
+++ lua/internal/devicon.lua
@@ -3,7 +3,9 @@ local M = {}
 local patterns_ft = require("icons.pattern")
 
 M.get_ext = function(name)
-	for pattern, ext in pairs(patterns_ft) do
+	for _, p_ft in pairs(patterns_ft) do
+		local pattern = p_ft[1]
+		local ext = p_ft[2]
 		if name:find(pattern) then
 			return ext
 		end
