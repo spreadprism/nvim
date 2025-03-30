9703546c79503b05d8f6773ae281dc298e1277f0
spreadprism <sprism@proton.me>
2025-03-30 19:08:12 -0400

    readded nvim config

---
+++ nix/configuration/nvim.nix
@@ -0,0 +1,14 @@
+{ nixpkgs, ...}:
+{
+  settings = {
+    wrapRc = true;
+    aliases = [];
+  };
+  categories = {
+    tmux = true;
+    ai = true;
+    git = true;
+    debugging = true;
+    testing = true;
+  };
+}
