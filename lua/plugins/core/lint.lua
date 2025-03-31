ec8c09fb9887b6c9ce168459d2fb53242f6bdb2f
spreadprism <sprism@proton.me>
2025-03-31 16:44:59 -0400

    added autoevent for lint

--- lua/plugins/core/lint.lua
+++ lua/plugins/core/lint.lua
@@ -4,4 +4,9 @@ plugin("nvim-lint"):on_require("lint"):after(function()
 		linters.go = { "golangcilint" }
 	end
 	require("lint").linters_by_ft = linters
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		callback = function()
+			require("lint").try_lint()
+		end,
+	})
 end)
