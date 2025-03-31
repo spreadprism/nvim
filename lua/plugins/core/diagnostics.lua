fa7149a084d479cc4a8124dae8d3601166940016
spreadprism <sprism@proton.me>
2025-03-31 11:42:25 -0400

    added diagnostics hover

--- lua/plugins/core/diagnostics.lua
+++ lua/plugins/core/diagnostics.lua
@@ -8,4 +8,16 @@ plugin("trouble.nvim")
 	:keys({
 		keymap("n", "<M-t>", keymapCmd("Trouble diagnostics toggle")),
 		keymap("n", "<M-T>", keymapCmd("Trouble diagnostics toggle filter.buf=0")),
+		keymap("n", "<M-d>", function()
+			vim.diagnostic.open_float({
+				border = "rounded",
+				scope = "line",
+				prefix = function(_, i, total)
+					if total == 1 then
+						return "", ""
+					end
+					return "(" .. i .. "/" .. total .. ") ", ""
+				end,
+			})
+		end),
 	})
