996df0df2fbb76dd2eba602f2f5fe1f84247630d
spreadprism <sprism@proton.me>
2025-03-31 11:21:07 -0400

    hopefully fixed keymaps for tmux

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,8 +1,8 @@
 if nixCats("tmux") then
 	keymapLoad({
-		keymap("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", "Navigate window left"),
-		keymap("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", "Navigate window down"),
-		keymap("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", "Navigate window up"),
-		keymap("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", "Navigate window right"),
+		keymap("n", "<C-h>", keymapCmd("TmuxNavigateLeft"), "Navigate window left"),
+		keymap("n", "<C-j>", keymapCmd("TmuxNavigateDown"), "Navigate window down"),
+		keymap("n", "<C-k>", keymapCmd("TmuxNavigateUp"), "Navigate window up"),
+		keymap("n", "<C-l>", keymapCmd("TmuxNavigateRight"), "Navigate window right"),
 	})
 end
