f204c53a3930f0592bad79b95d0025b14ea6a384
spreadprism <sprism@proton.me>
2025-04-01 11:40:57 -0400

    dap status

--- lua/plugins/ui/lualine_configs/sections/lualine_c.lua
+++ lua/plugins/ui/lualine_configs/sections/lualine_c.lua
@@ -1,15 +1,35 @@
 return {
 	{
 		function()
-			return require("dap").status()
-		end,
-		icon = { "", color = { fg = "#e7c664" } }, -- nerd icon.
-		cond = function()
-			if not package.loaded.dap then
-				return false
+			local ok, dap = pcall(require, "dap")
+			if not ok then
+				return " "
 			end
-			local session = require("dap").session()
-			return session ~= nil
+			local session = dap.session()
+			if session == nil then
+				return " "
+			end
+
+			return string.lower(dap.status())
 		end,
+		draw_empty = true,
+		icon = {
+			"",
+			color = function()
+				local ok, dap = pcall(require, "dap")
+				if not ok then
+					return ""
+				end
+				---@type dap.Session
+				local session = dap.session()
+				if session == nil then
+					return { fg = "#565f89" }
+				end
+				if not session.initialized then
+					return { fg = "#e7c664" }
+				end
+				return { fg = "#1abc9c" }
+			end,
+		}, -- nerd icon.
 	},
 }
