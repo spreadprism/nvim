0d7423eec48f4d3397f558892bb4eefbc8af3ae5
Emeric Duguay <emeric@everflow.io>
2025-04-02 09:54:22 -0400

    added range to diagnostic filter

--- lua/plugins/core/diagnostics.lua
+++ lua/plugins/core/diagnostics.lua
@@ -32,11 +32,15 @@ vim.diagnostic.set = function(namespace, bufnr, diagnostics, opts)
 		---@param diagnostic vim.Diagnostic
 		return vim.tbl_filter(function(diagnostic)
 			local key = string.format(
-				"%d_%s_%s_%s",
+				"%d_%s_%s_%s_%d_%d_%d_%d",
 				diagnostic.bufnr or 0,
 				diagnostic.code or "",
 				diagnostic.message,
-				diagnostic.source or "unknown"
+				diagnostic.source or "unknown",
+				diagnostic.col,
+				diagnostic.end_col or 0,
+				diagnostic.lnum,
+				diagnostic.end_lnum or 0
 			)
 			if unique[key] then
 				return false
