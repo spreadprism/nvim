27898744bf81f2fdc3e278934bc16a4e5c327e8c
spreadprism <sprism@proton.me>
2025-04-01 20:28:20 -0400

    added diagnostics filtr

--- lua/plugins/core/diagnostics.lua
+++ lua/plugins/core/diagnostics.lua
@@ -22,3 +22,29 @@ plugin("trouble.nvim")
 			})
 		end),
 	})
+
+local old_set = vim.diagnostic.set
+---@diagnostic disable-next-line: duplicate-set-field
+vim.diagnostic.set = function(namespace, bufnr, diagnostics, opts)
+	local filter = function(diagnostics)
+		local unique = {}
+
+		---@param diagnostic vim.Diagnostic
+		return vim.tbl_filter(function(diagnostic)
+			local key = string.format(
+				"%d_%s_%s_%s",
+				diagnostic.bufnr or 0,
+				diagnostic.code or "",
+				diagnostic.message,
+				diagnostic.source or "unknown"
+			)
+			if unique[key] then
+				return false
+			else
+				unique[key] = true
+				return true
+			end
+		end, diagnostics)
+	end
+	old_set(namespace, bufnr, filter(diagnostics), opts)
+end
