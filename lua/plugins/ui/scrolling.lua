0467f4b74cac948d2a12193db88ea47cbf0f5422
Emeric Duguay <emeric@everflow.io>
2025-04-03 10:18:46 -0400

    scrolling performance upgrade

--- lua/plugins/ui/scrolling.lua
+++ lua/plugins/ui/scrolling.lua
@@ -1,1 +1,15 @@
-plugin("neoscroll.nvim"):for_cat("core"):event_user():opts({ stop_eof = false })
+plugin("neoscroll.nvim"):for_cat("core"):event_user():opts({
+	stop_eof = false,
+	pre_hook = function()
+		vim.opt.eventignore:append({
+			"WinScrolled",
+			"CursorMoved",
+		})
+	end,
+	post_hook = function()
+		vim.opt.eventignore:remove({
+			"WinScrolled",
+			"CursorMoved",
+		})
+	end,
+})
