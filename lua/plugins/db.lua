8bf9f8239451105c3b42c2dc24066a18efc2c65d
Emeric Duguay <emeric@everflow.io>
2025-04-07 15:45:12 -0400

    added keymaps for dbee

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -9,16 +9,43 @@ plugin("nvim-dbee")
 				require("internal.db").source,
 			},
 			drawer = {
-				mappings = {},
+				mappings = {
+					{ key = "r", mode = "n", action = "refresh" },
+					-- INFO: action_1 opens a note or executes a helper
+					{ key = "<CR>", mode = "n", action = "action_1" }, -- INFO: open a note or execute a helper
+					{ key = "<F2>", mode = "n", action = "action_2" }, -- INFO: rename
+					{ key = "dd", mode = "n", action = "action_3" }, -- INFO: delete a note
+					{ key = "<Tab>", mode = "n", action = "toggle" },
+					{ key = "<CR>", mode = "n", action = "menu_confirm" },
+					{ key = "y", mode = "n", action = "menu_yank" },
+					{ key = "<Esc>", mode = "n", action = "menu_close" },
+					{ key = "<C-q>", mode = "n", action = "menu_close" },
+				},
 			},
 			result = {
-				mappings = {},
+				mappings = {
+					-- next/previous page
+					{ key = "yj", mode = "n", action = "yank_current_json" },
+					{ key = "yj", mode = "v", action = "yank_selection_json" },
+					{ key = "yJ", mode = "", action = "yank_all_json" },
+					{ key = "yc", mode = "n", action = "yank_current_csv" },
+					{ key = "yc", mode = "v", action = "yank_selection_csv" },
+					{ key = "yC", mode = "", action = "yank_all_csv" },
+					{ key = "<C-c>", mode = "", action = "cancel_call" },
+				},
 			},
 			editor = {
-				mappings = {},
+				mappings = {
+					{ key = "<Enter>", mode = "v", action = "run_selection" },
+					{ key = "<Enter>", mode = "n", action = "run_file" },
+					{ key = "<C-c>", mode = "", action = "cancel_call" },
+				},
 			},
 			call_log = {
-				mappings = {},
+				mappings = {
+					{ key = "<CR>", mode = "", action = "show_result" },
+					{ key = "<C-c>", mode = "", action = "cancel_call" },
+				},
 			},
 			window_layout = require("dbee.layouts").Default:new({
 				drawer_width = 30,
