485375a42fc0be9706188420e02227f4621998be
Emeric Duguay <emeric@everflow.io>
2025-04-08 12:19:06 -0400

    started working on diagnostics

--- lua/internal/telescope/diagnostics.lua
+++ lua/internal/telescope/diagnostics.lua
@@ -1,1 +1,9 @@
 -- TODO: https://www.youtube.com/watch?v=xdXE1tOT-qg
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+
+local M = {}
+
+M.setup = function() end
+
+return M
