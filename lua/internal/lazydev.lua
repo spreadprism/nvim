63cf6a6802b1763cf2c5e9b4ae750a1dd2145af3
spreadprism <sprism@proton.me>
2025-04-10 20:22:35 -0400

    added efficient cfg check

--- lua/internal/lazydev.lua
+++ lua/internal/lazydev.lua
@@ -6,7 +6,8 @@ M.paths = {
 	"nvim-dap-ui",
 }
 
-if exec("git config remote.origin.url"):gsub("\n", "") ~= "git@github.com:spreadprism/nvim.git" then
+local s = vim.split(cwd(), "/", { trimempty = true })
+if s[#s] ~= "nvim" then
 	table.insert(M.paths, (nixCats.configDir or "") .. "/lua/internal")
 end
 
