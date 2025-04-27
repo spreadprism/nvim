2ef746cfca61445ccde7d74b51cc1f4ea18c36c7
spreadprism <sprism@proton.me>
2025-04-27 17:34:23 -0400

    wip

--- lua/internal/dap_rust.lua
+++ lua/internal/dap_rust.lua
@@ -1,26 +1,5 @@
 local M = {}
 
---- generate overseer template
----@param config_name string
----@param cmd string
----@param args table
----@param cwd string
-local function generate_template(config_name, cmd, args, cwd)
-	local template_name = config_name .. "_prebuild"
-	local template = {
-		name = template_name,
-		builder = function()
-			return {
-				cmd = { cmd },
-				args = vim.list_extend(args, { "--message-format=json", ">>", joinpath(cwd(), ".buildmsg.json") }),
-				cwd = cwd,
-			}
-		end,
-	}
-
-	return template_name, template
-end
-
 function M.cargo(config)
 	local final_config = vim.deepcopy(config)
 	local cargo = final_config.cargo
@@ -52,29 +31,29 @@ function M.cargo(config)
 		local json = file:read("*a")
 
 		local executables = {}
-		vim
-			.iter(json:gmatch("([^\n]*)\n?'"))
-			:map(function(value)
-				local is_json, artifact = pcall(vim.fn.json_decode, value)
-				---@diagnostic disable-next-line: redundant-return-value
-				return is_json, artifact
-			end)
-			:filter(function(is_json, artifact)
-				-- only process artifact if it's valid json object and it is a compiler artifact
-				return is_json and type(artifact) == "table" and artifact.reason == "compiler-artifact"
-			end)
-			---@param artifact table
-			:each(function(_, artifact)
-				local is_binary = vim.list_contains(artifact.target.crate_types, "bin")
-				local is_build_script = vim.list_contains(artifact.target.kind, "custom-build")
-				local is_test = ((artifact.profile.test == true) and (artifact.executable ~= nil))
-					or vim.list_contains(artifact.target.kind, "test")
-				-- only add executable to the list if we want a binary debug and it is a binary
-				-- or if we want a test debug and it is a test
-				if (args[1] == "build" and is_binary and not is_build_script) or (args[1] == "test" and is_test) then
-					table.insert(executables, artifact.executable)
-				end
-			end)
+		-- vim
+		-- 	.iter(json:gmatch("([^\n]*)\n?'"))
+		-- 	:map(function(value)
+		-- 		local is_json, artifact = pcall(vim.fn.json_decode, value)
+		-- 		---@diagnostic disable-next-line: redundant-return-value
+		-- 		return is_json, artifact
+		-- 	end)
+		-- 	:filter(function(is_json, artifact)
+		-- 		-- only process artifact if it's valid json object and it is a compiler artifact
+		-- 		return is_json and type(artifact) == "table" and artifact.reason == "compiler-artifact"
+		-- 	end)
+		-- 	---@param artifact table
+		-- 	:each(function(_, artifact)
+		-- 		local is_binary = vim.list_contains(artifact.target.crate_types, "bin")
+		-- 		local is_build_script = vim.list_contains(artifact.target.kind, "custom-build")
+		-- 		local is_test = ((artifact.profile.test == true) and (artifact.executable ~= nil))
+		-- 			or vim.list_contains(artifact.target.kind, "test")
+		-- 		-- only add executable to the list if we want a binary debug and it is a binary
+		-- 		-- or if we want a test debug and it is a test
+		-- 		if (args[1] == "build" and is_binary and not is_build_script) or (args[1] == "test" and is_test) then
+		-- 			table.insert(executables, artifact.executable)
+		-- 		end
+		-- 	end)
 
 		if #executables <= 0 then
 			vim.notify("Multiple compilation artifacts are not supported.", vim.log.levels.ERROR)
@@ -90,6 +69,7 @@ function M.cargo(config)
 	end
 
 	final_config.cargo = nil
+	return final_config
 end
 
 return M
