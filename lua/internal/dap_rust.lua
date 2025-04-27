0ae27cf3a57a95a3672eeb25917663979ea78b7e
spreadprism <sprism@proton.me>
2025-04-27 18:01:03 -0400

    comment

--- lua/internal/dap_rust.lua
+++ lua/internal/dap_rust.lua
@@ -9,30 +9,30 @@ function M.cargo(config)
 
 	local template_name = final_config.name .. "_build"
 	local executable = ""
-	local template = {
-		name = template_name,
-		builder = function()
-			return {
-				cmd = { "cargo" },
-				args = vim.list_extend(args, { "--message-format=json" }),
-				cwd = cwd,
-				components = {
-					desc = "parse cargo build output",
-					editable = false,
-					serializable = false,
-					constructor = function()
-						return {
-							on_result = function(self, task, result)
-								vim.print(result)
-							end,
-						}
-					end,
-				},
-			}
-		end,
-	}
-
-	require("overseer").register_template(template)
+	-- local template = {
+	-- 	name = template_name,
+	-- 	builder = function()
+	-- 		return {
+	-- 			cmd = { "cargo" },
+	-- 			args = vim.list_extend(args, { "--message-format=json" }),
+	-- 			cwd = cwd,
+	-- 			components = {
+	-- 				desc = "parse cargo build output",
+	-- 				editable = false,
+	-- 				serializable = false,
+	-- 				constructor = function()
+	-- 					return {
+	-- 						on_result = function(self, task, result)
+	-- 							vim.print(result)
+	-- 						end,
+	-- 					}
+	-- 				end,
+	-- 			},
+	-- 		}
+	-- 	end,
+	-- }
+	--
+	-- require("overseer").register_template(template)
 	final_config.preLaunchTask = template_name
 	final_config.program = function()
 		return executable
