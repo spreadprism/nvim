0e7f0d522105f260e44c3b8bd4295178837e9cac
spreadprism <sprism@proton.me>
2025-04-27 14:17:26 -0400

    wip -- cargo dap support

--- lua/language/rust.lua
+++ lua/language/rust.lua
@@ -4,20 +4,18 @@ end
 lsp("rust_analyzer")
 linter("rust", "clippy")
 formatter("rust", "rustfmt")
-for _, adapter in ipairs({ "rust", "codelldb", "lldb" }) do
-	dap(adapter, {
-		type = "server",
-		port = "${port}",
-		executable = {
-			command = "codelldb",
-			args = { "--port", "${port}" },
-		},
-		enrich_config = function(config, on_config)
-			-- If the configuration(s) in `launch.json` contains a `cargo` section
-			-- send the configuration off to the cargo_inspector.
-			if config["cargo"] ~= nil then
-				on_config(require("internal.dap_rust").cargo_inspector(config))
-			end
-		end,
-	})
-end
+dap("rust", {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = "codelldb",
+		args = { "--port", "${port}" },
+	},
+	-- enrich_config = function(config, on_config)
+	-- 	-- If the configuration(s) in `launch.json` contains a `cargo` section
+	-- 	-- send the configuration off to the cargo_inspector.
+	-- 	if config["cargo"] ~= nil then
+	-- 		on_config(require("internal.dap_rust").cargo(config))
+	-- 	end
+	-- end,
+})
