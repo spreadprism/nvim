7658ce3dc97ef587f2fde1593458a7bbd4eea00c
spreadprism <sprism@proton.me>
2025-04-27 13:40:31 -0400

    added adapters

--- lua/language/rust.lua
+++ lua/language/rust.lua
@@ -4,13 +4,13 @@ end
 lsp("rust_analyzer")
 linter("rust", "clippy")
 formatter("rust", "rustfmt")
-local opts = {
-	type = "server",
-	port = "13000",
-	executable = {
-		command = "codelldb",
-		args = { "--port", "13000" },
-	},
-}
-dap("codelldb", opts)
-dap("lldb", opts)
+for _, adapter in ipairs({ "rust", "codelldb", "lldb" }) do
+	dap(adapter, {
+		type = "server",
+		port = "13000",
+		executable = {
+			command = "codelldb",
+			args = { "--port", "13000" },
+		},
+	})
+end
