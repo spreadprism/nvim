97a2b76705d2157d92c201b754af7a0657bc1bca
spreadprism <sprism@proton.me>
2025-04-27 13:49:48 -0400

    attempt to add rust support

--- lua/language/rust.lua
+++ lua/language/rust.lua
@@ -7,10 +7,17 @@ formatter("rust", "rustfmt")
 for _, adapter in ipairs({ "rust", "codelldb", "lldb" }) do
 	dap(adapter, {
 		type = "server",
-		port = "13000",
+		port = "${port}",
 		executable = {
 			command = "codelldb",
-			args = { "--port", "13000" },
+			args = { "--port", "${port}" },
 		},
+		enrich_config = function(config, on_config)
+			-- If the configuration(s) in `launch.json` contains a `cargo` section
+			-- send the configuration off to the cargo_inspector.
+			if config["cargo"] ~= nil then
+				on_config(require("internal.dap_rust").cargo_inspector(config))
+			end
+		end,
 	})
 end
