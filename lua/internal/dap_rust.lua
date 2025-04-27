e6b8ebb66584c96e6080b78465335962f0480d8b
spreadprism <sprism@proton.me>
2025-04-27 17:58:45 -0400

    attempt cargo

--- lua/internal/dap_rust.lua
+++ lua/internal/dap_rust.lua
@@ -9,6 +9,7 @@ function M.cargo(config)
 
 	local template_name = final_config.name .. "_build"
 	local msg_path = joinpath(cwd, template_name .. "_msg.json")
+	local executable = ""
 	local template = {
 		name = template_name,
 		builder = function()
@@ -17,17 +18,16 @@ function M.cargo(config)
 				args = vim.list_extend(args, { "--message-format=json" }),
 				cwd = cwd,
 				components = {
-					{
-						desc = "grab output",
-						serializable = false,
-						constructor = function(params)
-							return {
-								on_output_lines = function(self, task, lines)
-									print(lines)
-								end,
-							}
-						end,
-					},
+					desc = "parse cargo build output",
+					editable = false,
+					serializable = false,
+					constructor = function()
+						return {
+							on_result = function(self, task, result)
+								vim.print(result)
+							end,
+						}
+					end,
 				},
 			}
 		end,
@@ -36,49 +36,7 @@ function M.cargo(config)
 	require("overseer").register_template(template)
 	final_config.preLaunchTask = template_name
 	final_config.program = function()
-		local file = io.open(msg_path, "r")
-		if file == nil then
-			return require("dap").ABORT
-		end
-		---@type string
-		local json = file:read("*a")
-
-		local executables = {}
-		-- vim
-		-- 	.iter(json:gmatch("([^\n]*)\n?'"))
-		-- 	:map(function(value)
-		-- 		local is_json, artifact = pcall(vim.fn.json_decode, value)
-		-- 		---@diagnostic disable-next-line: redundant-return-value
-		-- 		return is_json, artifact
-		-- 	end)
-		-- 	:filter(function(is_json, artifact)
-		-- 		-- only process artifact if it's valid json object and it is a compiler artifact
-		-- 		return is_json and type(artifact) == "table" and artifact.reason == "compiler-artifact"
-		-- 	end)
-		-- 	---@param artifact table
-		-- 	:each(function(_, artifact)
-		-- 		local is_binary = vim.list_contains(artifact.target.crate_types, "bin")
-		-- 		local is_build_script = vim.list_contains(artifact.target.kind, "custom-build")
-		-- 		local is_test = ((artifact.profile.test == true) and (artifact.executable ~= nil))
-		-- 			or vim.list_contains(artifact.target.kind, "test")
-		-- 		-- only add executable to the list if we want a binary debug and it is a binary
-		-- 		-- or if we want a test debug and it is a test
-		-- 		if (args[1] == "build" and is_binary and not is_build_script) or (args[1] == "test" and is_test) then
-		-- 			table.insert(executables, artifact.executable)
-		-- 		end
-		-- 	end)
-
-		if #executables <= 0 then
-			vim.notify("Multiple compilation artifacts are not supported.", vim.log.levels.ERROR)
-			return
-		end
-		if #executables > 1 then
-			vim.notify("Multiple compilation artifacts are not supported.", vim.log.levels.ERROR)
-			return
-		end
-
-		exec("rm " .. msg_path)
-		return executables[1]
+		return executable
 	end
 
 	final_config.cargo = nil
