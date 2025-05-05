46c90887626bc0c330d70ceff9945683f845c2b1
spreadprism <sprism@proton.me>
2025-05-05 16:25:32 -0400

    modified overseer keybinds

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -38,8 +38,17 @@ plugin("overseer")
 	:cmd({ "OverseerRun", "OverseerToggle", "OverseerQuickAction", "OverseerRunCmd" })
 	:keys({
 		kgroup("<leader>t", "tasks", {}, {
-			kmap("n", "t", kcmd("OverseerToggle"), "Toggle UI"),
-			kmap("n", "r", kcmd("OverseerRun"), "Task run"),
-			kmap("n", "l", kcmd("OverseerQuickAction open float"), "Open float last task"),
+			kmap("n", "e", kcmd("OverseerToggle"), "Toggle explorer"),
+			kmap("n", "t", kcmd("OverseerRun"), "Task run"),
+			kmap("n", "r", function()
+				local overseer = require("overseer")
+				local tasks = overseer.list_tasks({ recent_first = true })
+				if vim.tbl_isempty(tasks) then
+					vim.notify("No tasks found", vim.log.levels.WARN)
+				else
+					overseer.run_action(tasks[1], "restart")
+				end
+			end, "rerun last task"),
+			kmap("n", "f", kcmd("OverseerQuickAction open float"), "Open float last task"),
 		}),
 	})
