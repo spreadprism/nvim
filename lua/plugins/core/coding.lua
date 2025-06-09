593cfc0855b8d49611feacf8b774a9d1d7508aa2
spreadprism <sprism@proton.me>
2025-06-09 18:05:56 -0400

    added better comment highlighting

--- lua/plugins/core/coding.lua
+++ lua/plugins/core/coding.lua
@@ -24,7 +24,12 @@ plugin("mini.indentscope"):event_defer():init(function()
 end)
 plugin("todo-comments.nvim"):on_require("todo-comments"):event_defer():opts({
 	highlight = {
-		multiline = false,
+		-- vimgrep regex, supporting the pattern TODO(name):
+		pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]],
+	},
+	search = {
+		-- ripgrep regex, supporting the pattern TODO(name):
+		pattern = [[\b(KEYWORDS)(\(\w*\))*:]],
 	},
 })
 plugin("nvim-highlight-colors"):event_defer():opts({
