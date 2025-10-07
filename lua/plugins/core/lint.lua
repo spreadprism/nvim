1c6ceeb39fc283889d634067e241ac75d80ee4c7
Emeric Duguay <emeric@everflow.io>
2025-10-07 11:48:07 -0400

    better support for linting monorepos

--- lua/plugins/core/lint.lua
+++ lua/plugins/core/lint.lua
@@ -1,6 +1,23 @@
+local function get_client()
+	local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+	clients = vim.tbl_filter(function(c)
+		return c.name ~= "copilot" and c.name ~= "ast_grep"
+	end, clients)
+
+	return clients[1] or {}
+end
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
 	callback = function()
-		require("lint").try_lint()
+		local client = get_client()
+		if not client.initialized then
+			vim.defer_fn(function()
+				client = get_client()
+				require("lint").try_lint(nil, { cwd = client.root_dir or cwd() })
+			end, 100)
+		else
+			require("lint").try_lint(nil, { cwd = client.root_dir or cwd() })
+		end
 	end,
 })
 plugin("nvim-lint")
