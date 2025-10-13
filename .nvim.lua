ba30e129334dae813b275f8a9b4f074321707004
spreadprism <sprism@proton.me>
2025-10-13 16:15:27 -0400

    removed base cfg

--- .nvim.lua
+++ .nvim.lua
@@ -1,11 +1,1 @@
 workspace.init()
-
-workspace.launch_configs_ft("go", {
-	name = "debug",
-	program = joinpath(cwd(), "test", "main.go"),
-})
-
-workspace.launch_configs_ft("python", {
-	name = "debug python",
-	program = joinpath(cwd(), "test", "test.py"),
-})
