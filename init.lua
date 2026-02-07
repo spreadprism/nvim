ea6dc77d82ba9b2da895d87178bbc4fabfbd441b
spreadprism <sprism@proton.me>
2026-02-07 17:38:02 -0500

    modified init

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ vim.loader.enable()
 
 for _, path in ipairs(vim.api.nvim_get_runtime_file("*/inits/*.lua", true)) do
 	local name = vim.fn.fnamemodify(path, ":t:r")
-  local module_path = "inits." .. name
+	local module_path = "inits." .. name
 	local ok, msg = pcall(require, module_path)
-  if not ok then
-    print("Error loading init file: " .. module_path .. " (cause=" .. msg .. ")")
-  end
+	if not ok then
+		print("Error loading init file: " .. module_path .. " (cause=" .. msg .. ")")
+	end
 end
