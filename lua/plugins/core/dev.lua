069a3c37172218e6a57ed23e140005106d977ab0
spreadprism <sprism@proton.me>
2026-04-14 17:29:50 -0400

    fixed startup time plugin

--- lua/plugins/core/dev.lua
+++ lua/plugins/core/dev.lua
@@ -1,3 +1,9 @@
-plugin("startuptime"):opts(false):cmd("StartupTime"):keymaps({
-	k:map("n", "<leader>==", k:cmd("StartupTime"), "startup time"),
-})
+plugin("startuptime")
+	:opts(false)
+	:cmd("StartupTime")
+	:keymaps({
+		k:map("n", "<leader>==", k:cmd("tab StartupTime"), "startup time"),
+	})
+	:before(function()
+		vim.g.startuptime_exe_path = "nvim"
+	end)
