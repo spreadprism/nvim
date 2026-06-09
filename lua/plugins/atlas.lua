73cbf7b46b8cce95f8da9bacab181757f1e982be
Emeric Duguay <emeric@everflow.io>
2026-06-09 11:28:18 -0400

    added cfg for jira

--- lua/plugins/atlas.lua
+++ lua/plugins/atlas.lua
@@ -1,18 +1,30 @@
 plugin("atlas")
-	:opts({
-		pulls = {
-			diff = {
-				open_cmd = "CodeDiff",
+	:opts(function()
+		local base = {
+			pulls = {
+				diff = {
+					open_cmd = "CodeDiff",
+				},
+				providers = {
+					github = {}, -- See configuration below
+				},
 			},
-			providers = {
-				github = {}, -- See configuration below
+			issues = {
+				providers = {
+					github = {}, -- See configuration below
+				},
 			},
-		},
-		issues = {
-			providers = {
-				jira = {}, -- See configuration below
-				github = {}, -- See configuration below
-			},
-		},
-	})
+		}
+		local jira_base_url = vim.env.JIRA_BASE_URL
+		local jira_email = vim.env.JIRA_EMAIL
+		local jira_api_token = vim.env.JIRA_API_TOKEN
+		if jira_base_url and jira_email and jira_api_token then
+			base.issues.providers.jira = {
+				base_url = jira_base_url,
+				email = jira_email,
+				api_token = jira_api_token,
+			}
+		end
+		return base
+	end)
 	:event("DeferredUIEnter")
