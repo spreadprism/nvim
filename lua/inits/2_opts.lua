0d3cb0bfecb129d17f418b464e64967c55765b16
spreadprism <sprism@proton.me>
2026-09-07 17:43:22 -0400

    refactor

--- lua/inits/2_opts.lua
+++ lua/inits/2_opts.lua
@@ -35,9 +35,9 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 -- per workspace shadafile
 vim.opt.exrc = true
 vim.opt.secure = true
+
 local workspace_path = vim.fn.getcwd()
 local cache_dir = vim.fn.stdpath("data")
 local unique_id = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
 local shadafile = cache_dir .. "/myshada/" .. unique_id .. ".shada"
-
 vim.opt.shadafile = shadafile
