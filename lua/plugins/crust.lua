becb51fc86964619c46fc3a8a1464316a276f08b
spreadprism <sprism@proton.me>
2026-09-20 17:17:52 -0400

    chore: update crust

--- lua/plugins/crust.lua
+++ lua/plugins/crust.lua
@@ -11,6 +11,7 @@ plugin("crust")
 	})
 	:keymaps({
 		k:map("nx", "<M-p>", k:require("crust").smart(), "crust"),
+		k:map("n", "<localleader>m", k:require("crust").model(), "model"):ft(ft),
 		k:map("n", "<localleader>s", k:require("crust").sessions(), "session"):ft(ft),
 		k:map("n", "<localleader>l", k:require("crust").session_last(), "last session"):ft(ft),
 		k:map("n", "<localleader>n", k:require("crust").new_session(), "new session"):ft(ft),
