0c667ed3f9743c40a16a05a75ba2dab9a98be5d4
spreadprism <sprism@proton.me>
2026-09-23 15:18:05 -0400

    remove unused binding

--- lua/plugins/core/git.lua
+++ lua/plugins/core/git.lua
@@ -28,11 +28,6 @@ plugin("neogit")
 	:cmd("Neogit")
 	:keymaps({
 		k:group("git", "<leader>g", {
-			k:map("n", "b", k:cmd("Neogit branch"), "branch"),
-			k:map("n", "l", k:cmd("Neogit log"), "log"),
-			k:map("n", "p", k:cmd("Neogit pull"), "pull"),
-			k:map("n", "P", k:cmd("Neogit push"), "push"),
-			k:map("n", "c", k:cmd("Neogit commit"), "commit"),
 			k:map("n", "g", function()
 				vim.cmd("tablast")
 				local ft = vim.bo.filetype
